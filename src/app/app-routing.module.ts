828647c449c21f919fae9377f3018b92b57f6423
jesus19951206@gmail.com <jesus19951206@gmail.com>
2022-02-15 19:37:20 -0600

    last changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmpleadoComponent } from './COMPONENTS/empleado/empleado.component';
-import { DetailUserComponent } from './COMPONENTS/detail-user/detail-user.component'; 
+import { DetailUserComponent } from './COMPONENTS/detail-user/detail-user.component';
 const routes: Routes = [
   {path:'',redirectTo:'empleado',pathMatch:'full'},
   {path:'empleado',component:EmpleadoComponent},
